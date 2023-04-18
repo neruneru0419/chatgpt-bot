21c981ec9a9030dacab09bee85cdf463d1a7b153
neruneru0419 <neruneru55555@gmail.com>
2023-04-18 23:57:28 +0900

    discordに送信されたメッセージを変数に格納

--- src/main.rb
+++ src/main.rb
@@ -12,10 +12,11 @@ settings_text = File.open("setting.txt", "r").read
 message_historys = []
 bot.message do |event|
   if event.channel.name == ENV['NAME']
-    chatgpt_content = create_text(message_historys, event.message.content, settings_text)
+    user_message = event.message.content
+    chatgpt_content = create_text(message_historys, user_message, settings_text)
     event.send_message(chatgpt_content)
 
-    message_historys.push(event.message.content)
+    message_historys.push(user_message)
     message_historys.push(chatgpt_content)
     if message_historys.size >= 8
       message_historys.shift
